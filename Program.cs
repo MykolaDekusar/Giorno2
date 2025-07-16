72ad70bf7aafcf2b6a1b23a0e4da66acef6129a3
MykolaDekusar <ndekusar97@gmail.com>
2025-07-16 16:43:27 +0200

    Aggiunta richiesta di operatore

--- Program.cs
+++ Program.cs
@@ -6,9 +6,12 @@ class Program
     {
         Console.Write("Inserisci il primo numero: ");
         double numero1 = double.Parse(Console.ReadLine());
-       
+
         Console.Write("Inserisci il secondo numero: ");
         double numero2 = double.Parse(Console.ReadLine());
-       
+
+        Console.Write("Scegli l'operazione da fare(+, -, /, *): ");
+        string operatore = Console.ReadLine();
+
     }
 }
