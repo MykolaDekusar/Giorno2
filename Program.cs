54c6bc16595be614c9084957eaf4bbc12086ed10
MykolaDekusar <ndekusar97@gmail.com>
2025-07-16 16:37:20 +0200

    Imput utente e conversioe stringa in numero C#

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Inserisci il primo numero: ");
+        double numero1 = double.Parse(Console.ReadLine());
+       
+        Console.Write("Inserisci il secondo numero: ");
+        double numero2 = double.Parse(Console.ReadLine());
+       
+    }
+}
